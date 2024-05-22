0a7f7c5ea33d75a7ac24cf33bae1933de3802afe
Mallela Sindhuja <92076159+HudsinGod@users.noreply.github.com>
2024-05-22 23:46:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,20 @@
 const numClouds = 10;
 
 for(let i = 0; i < numClouds; i++) {
-    // Create a new cloud element
+    // Creating new cloud
     let cloud = document.createElement('div');
     cloud.className = 'cloud';
     cloud.innerHTML = '<div class="cloudBubble1"></div><div class="cloudBubble2"></div>';
 
-    // Append the new cloud to the container
+    // Appending new cloud to container
     document.getElementById('container').appendChild(cloud);
-
-    // Set initial position and scale
+    
     cloud.style.left = Math.random() * window.innerWidth + 'px';
     cloud.style.top = Math.random() * window.innerHeight + 'px';
     cloud.style.transform = 'scale(' + (Math.random() * 0.5 + 0.5) + ')';
 
-    // Set transition
     cloud.style.transition = 'all linear ' + (Math.random() * 5 + 5) + 's';
 
-    // Start animation
     setTimeout(moveCloud.bind(null, cloud), Math.random() * 2000);
 }
 
@@ -26,6 +23,5 @@ function moveCloud(cloud) {
     cloud.style.left = Math.random() * window.innerWidth + 'px';
     cloud.style.top = Math.random() * window.innerHeight + 'px';
 
-    // Repeat animation
     setTimeout(moveCloud.bind(null, cloud), Math.random() * 5000 + 5000);
 }
